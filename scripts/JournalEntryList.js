c0c143ee6697bf66156cc91b587a75ad3ca85450
Raparker88 <raparker88@gmail.com>
2020-07-23 15:25:46 -0500

    switched for loop for map method

--- scripts/JournalEntryList.js
+++ scripts/JournalEntryList.js
@@ -1,17 +1,14 @@
 import { createEntryHTML } from "./JournalEntry.js"
 import { useJournalEntries } from "./JournalDataProvider.js"
 
-let entryListHTML = ""
+
 
 export const entryList = () => {
     const contentElement = document.querySelector(".past__entries")
     const entries = useJournalEntries()
 
-    for (const entry of entries) {
-        entryListHTML += createEntryHTML(entry)
-    }
 
-    contentElement.innerHTML += `
-        ${entryListHTML}
-    `
+
+    contentElement.innerHTML = entries.map(entry => createEntryHTML(entry)).join("")
+    
 }
